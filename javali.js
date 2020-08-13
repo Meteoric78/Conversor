da4068ab2b897b6928885ed91dd94bb916431d45
Meteoric78 <edvanfilho39@gmail.com>
2020-08-13 12:27:15 -0300

    Rework

--- javali.js
+++ javali.js
@@ -1,9 +1,19 @@
 
 
 function convertFc(input) {
-  var input = document.getElementById("num").value;
-  var resultado = document.getElementById("resul");
- var cel = (input - 32) * 5 / 9;
- resultado.innerHTML = cel;
-
+ // Pega o número inserido, calcula usando a fórmula e exibe o resultado 
+    var input = document.getElementById("num").value;
+ // var resultado = document.getElementById("resul");
+ // var cel = (input - 32) * 5 / 9;
+ // resultado.innerHTML = cel;
+ // Exibe passo a passo da conta
+  var semP = input - 32;
+  var multP = semP * 5;
+  var res = multP / 9;
+  document.getElementById("p0").innerHTML = "Resolução:";
+  document.getElementById("p1").innerHTML = "°C = (°F - 32) x 5 / 9";
+  document.getElementById("p2").innerHTML = "°C = (" + input + " - 32) * 5 / 9";
+  document.getElementById("p3").innerHTML = "°C = " + semP + " * 5 / 9";
+  document.getElementById("p4").innerHTML = "°C = " + multP + " / 9";
+  document.getElementById("p5").innerHTML = res;
    };
--- t.js
+++ t.js
@@ -1,6 +1,15 @@
 function convertCf(inp) {
     var inp = document.getElementById("num").value;
-    var resultado = document.getElementById("resul");
-    var lec = (inp * 9/5) + 32;
-    resultado.innerHTML = lec;
+  //  var resultado = document.getElementById("resul");
+  //  var lec = (inp * 9/5) + 32;
+  //  resultado.innerHTML = lec;
+  var semP = inp * 9;
+  var multP = semP / 5;
+  var res = multP + 32;
+  document.getElementById("p0").innerHTML = "Resolução:";
+  document.getElementById("p1").innerHTML = "°F = (°C * 9 / 5) + 32";
+  document.getElementById("p2").innerHTML = "°F = (" + inp + " * 9 / 5) + 32";
+  document.getElementById("p3").innerHTML = "°F = (" + semP + " / 5) + 32";
+  document.getElementById("p4").innerHTML = "°F = " + multP + " + 32";
+  document.getElementById("p1").innerHTML = res;
   };
